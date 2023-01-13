f819ae1389b3de790b2cec26d6deb93041c258bc
E_Antonio23 <eantoniovf@gmail.com>
2023-01-12 23:45:02 -0600

    creacion dir js mv holamundojs

---
+++ js/01-hola-mundo.js
@@ -0,0 +1,5 @@
+alert("Hola Mundo con JS");
+alert("Bienvenido a mi pagina web");
+document.write("Hola Mundo desde fichero externo");
+console.log("Muestra esto en la consola");
+console.log(88+4);
