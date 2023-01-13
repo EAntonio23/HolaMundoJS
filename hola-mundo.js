9a094a0dbfa04c0d5bfe8936d4af04abbbb8f87f
E_Antonio23 <eantoniovf@gmail.com>
2023-01-13 00:01:04 -0600

    variables y comentarios

--- hola-mundo.js
+++
@@ -1,5 +0,0 @@
-alert("Hola Mundo con JS");
-alert("Bienvenido a mi pagina web");
-document.write("Hola Mundo desde fichero externo");
-console.log("Muestra esto en la consola");
-console.log(88+4);
---
+++ js/02-variables.js
@@ -0,0 +1,13 @@
+//VARIABLES
+//Una variable es un contenedor de información
+
+var pais = "Mexico";
+var continente = "America";
+var antiguedad = 2023;
+var pais_y_continente = pais + ' ' + continente;
+
+pais = "España";
+continente = "Europa";
+
+console.log(pais, continente, antiguedad);
+alert(pais_y_continente);
