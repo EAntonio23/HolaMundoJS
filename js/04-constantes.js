1c795bbd818e91de2b96ca7c12c614c54eb43358
E_Antonio23 <eantoniovf@gmail.com>
2023-01-13 23:19:21 -0600

    se agrego fichero y ejemplo constantes

---
+++ js/04-constantes.js
@@ -0,0 +1,12 @@
+'use strict'
+
+//Constantes
+//Como una variable pero su valor no puede cambiar
+
+var web = "https://google.com";
+const ip = "192.168.1.254";
+
+web = "https://facebook.com";
+ip = "192.168.2.254";
+
+console.log(web, ip);
